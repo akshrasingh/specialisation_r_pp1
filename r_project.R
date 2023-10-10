b11e9341cbe8ec6325bec1cd9d2ca26f5ba60951
Akshra Singh <110145062+akshrasingh@users.noreply.github.com>
2023-10-10 13:15:56 +0530

    second commit

--- r_project.R
+++ r_project.R
@@ -1,6 +1,8 @@
 library(tidyr)
 library(dplyr)
+#tidyr and dplyr for data manipulation 
 install.packages("caret")
+#caret for ml 
 library(caret)
 
 library(rpart)
@@ -11,6 +13,7 @@ install.packages("ROCR")
 library(ROCR)
 install.packages("gbm")
 library(gbm)
+#corrplot for corelation matrix 
 install.packages("corrplot")
 library(corrplot)
 library(stringr)
@@ -38,16 +41,18 @@ summary(clean_data)
 numeric_data <- clean_data[, sapply(clean_data, is.numeric)]
 
 # Correlation matrix
-if (ncol(numeric_data) > 1) { # check if we have at least two numeric variables
-  cor_matrix <- cor(numeric_data, method = "pearson")
-  print("Correlation Matrix:")
-  print(cor_matrix)
+if (ncol(numeric_data) > 1) { # Check if we have at least two numeric variables
+  cor_matrix <- cor(numeric_data, method = "pearson") # Calculate the Pearson correlation matrix for numeric variables
+  print("Correlation Matrix:") # Print a message indicating that the following output is the correlation matrix
+  print(cor_matrix) # Print the calculated correlation matrix
+  
   # Visualize correlation matrix
-  library(corrplot)
-  corrplot(cor_matrix, method = "circle")
+  library(corrplot) # Load the corrplot package for visualization
+  corrplot(cor_matrix, method = "circle") # Visualize the correlation matrix using circular plot method
 } else {
-  print("Not enough numeric variables for a correlation matrix.")
+  print("Not enough numeric variables for a correlation matrix.") # Print a message if there are not enough numeric variables
 }
+
 
 
 
@@ -60,4 +65,39 @@ clean_data %>%
   ggpairs(columns = c("age", "creatinine_phosphokinase", "ejection_fraction", "platelets", "serum_creatinine", "serum_sodium"),
           mapping = ggplot2::aes(color = DEATH_EVENT)) +
   ggplot2::theme_light()
+
+
+# Age distribution Plots
+
+# Simple Age distribution
+clean_data %>%
+  ggplot(aes(x = age)) + 
+  geom_histogram(binwidth = 5, 
+                 color = "grey", 
+                 alpha = 0.5) +
+  labs(title = "Age Distribution") +
+  scale_x_continuous(breaks = seq(40,100,10))
+
+# Age distribution against Death Event
+clean_data %>% 
+  ggplot(aes(x = age, fill = DEATH_EVENT)) +
+  geom_histogram(binwidth = 5, 
+                 position = "identity",
+                 alpha = 0.5,color = "white") +
+  scale_fill_manual(values = c("#999999", "#1F77B4")) +
+  labs(title = "Age Distribution with Death Event")+
+  scale_x_continuous(breaks = seq(40,100,10))
+
+
+# Boxplot of Age by Death Event
+ggplot(clean_data, aes(x = DEATH_EVENT, y = age, fill = DEATH_EVENT)) +
+  geom_boxplot() +
+  labs(title = "Box Plot of Age by Death Event") +
+  scale_fill_brewer(palette = "Set1")
+
+
+# Boxplot of Age by Sex
+clean_data %>%
+  ggplot(aes(x = as.factor(sex), y = age)) + geom_boxplot(aes(fill = as.factor(sex))) +
+  labs(title = "Boxplot of Age by Sex") + xlab("Sex") + ylab("Age")
 
